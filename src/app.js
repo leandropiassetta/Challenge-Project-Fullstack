a260c86479be78cc4f83cf017ef7f8774014dad5
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-12-07 14:51:18 -0300

    configurando a model e criando o database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require("method-override");
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 
 app.set('view engine', 'ejs');
---
+++ src/config/database.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: "leandro",
+    password: "*Leandro86",
+    database: "Trafico",
+    host: "localhost",
+    dialect: "mysql",
+  },
+  test: {
+    username: "leandro",
+    password: "*Leandro86",
+    database: "Trafico_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "leandro",
+    password: "*Leandro86",
+    database: "Trafico_prod",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
